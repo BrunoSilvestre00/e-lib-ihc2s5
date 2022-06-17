8dc21886bf6a3ed09ad7d2410ae722f0bf997d18
Brunosilvestre00 <bruno.ap.silvestre@gmail.com>
2022-06-17 15:49:51 -0300

    Primeira implementação da tela Home

--- servidor_flask.py
+++ servidor_flask.py
@@ -12,7 +12,11 @@ def book_detail(book_id):
 
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    CAROUSEL_LIST = ['img_ex1.jpg', 'img_ex2.jpg', 'img_ex3.jpg']
+    BOOKS_FOR_YOU = [BOOKS[f'book_{i}'] for i in [7, 9, 11, 13, 14, 18]]
+    BOOKS_HORROR = [BOOKS[f'book_{i}'] for i in [4, 5, 6, 10, 15, 19]]
+    BOOKS_HAPPY = [BOOKS[f'book_{i}'] for i in [1, 3, 8, 12, 16, 17]]
+    return render_template('home.html', carousel_list=CAROUSEL_LIST, for_you=BOOKS_FOR_YOU, horror=BOOKS_HORROR, happy=BOOKS_HAPPY)
 
 if __name__ == '__main__':
     # webbrowser.open('http://127.0.0.1:5000/book-detail/1')
---
+++ static/scripts/home.js
@@ -0,0 +1,77 @@
+let slider_for_you = tns({
+    container: ".book-slider-for-you",
+    "slideBy": 1,
+    "speed": 400,
+    "nav": false,
+    controlsContainer: "#controls-for-you",
+    responsive: {
+        1600: {
+            items: 6,
+            gutter: 20
+        },
+        1024: {
+            items: 5,
+            gutter: 20
+        },
+        768: {
+            items: 3,
+            gutter: 20
+        },
+        480: {
+            items: 2,
+            gutter: 20
+        }
+    }
+})
+
+let slider_horror = tns({
+    container: ".book-slider-horror",
+    "slideBy": 1,
+    "speed": 400,
+    "nav": false,
+    controlsContainer: "#controls-horror",
+    responsive: {
+        1600: {
+            items: 6,
+            gutter: 20
+        },
+        1024: {
+            items: 5,
+            gutter: 20
+        },
+        768: {
+            items: 3,
+            gutter: 20
+        },
+        480: {
+            items: 2,
+            gutter: 20
+        }
+    }
+})
+
+let slider_happy = tns({
+    container: ".book-slider-happy",
+    "slideBy": 1,
+    "speed": 400,
+    "nav": false,
+    controlsContainer: "#controls-happy",
+    responsive: {
+        1600: {
+            items: 6,
+            gutter: 20
+        },
+        1024: {
+            items: 5,
+            gutter: 20
+        },
+        768: {
+            items: 3,
+            gutter: 20
+        },
+        480: {
+            items: 2,
+            gutter: 20
+        }
+    }
+})
