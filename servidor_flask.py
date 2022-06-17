4d8ba53f6ea51e05dc56482a2348b4ceb76447c4
Brunosilvestre00 <bruno.ap.silvestre@gmail.com>
2022-06-16 23:21:57 -0300

    Implementação visual do Header Principal

--- servidor_flask.py
+++ servidor_flask.py
@@ -3,14 +3,20 @@ import json
 import webbrowser
 
 app = Flask(__name__)
+BOOKS = None
 
 @app.route('/book-detail/<book_id>')
 def book_detail(book_id):
     book = BOOKS[f'book_{book_id}']
     return render_template('book_detail.html', book=book)
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
 if __name__ == '__main__':
-    webbrowser.open('http://127.0.0.1:5000/book-detail/1')
+    # webbrowser.open('http://127.0.0.1:5000/book-detail/1')
+    webbrowser.open('http://127.0.0.1:5000/home')
     with open('resources-data.json', 'r', encoding='utf-8') as f:
         BOOKS = json.load(f)
     app.run()
